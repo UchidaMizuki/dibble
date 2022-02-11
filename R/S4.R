fb409ac146ad11f5f997b77a5ce2d1f74f986cf6
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-02-12 08:26:29 +0900

    Update

--- R/S4.R
+++
@@ -1,5 +0,0 @@
-setOldClass("ddf_col")
-setOldClass("tbl_ddf")
-setOldClass("grouped_ddf")
-
-setClassUnion("dibble", c("ddf_col", "tbl_ddf", "grouped_ddf", "array", "vector"))
--- R/extremes.R
+++ R/extremes.R
@@ -1,5 +1,8 @@
-#' @include S4.R
-NULL
+setOldClass("ddf_col")
+setOldClass("tbl_ddf")
+setOldClass("grouped_ddf")
+
+setClassUnion("dibble", c("ddf_col", "tbl_ddf", "grouped_ddf", "array", "vector"))
 
 #' @export
 setGeneric("pmax",
