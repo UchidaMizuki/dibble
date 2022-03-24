cbd7a0da83fefb162db11c2d71213cde16b6a32b
UchidaMizuki <uchidamizuki@vivaldi.net>
2022-03-25 01:12:58 +0900

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(...) {
   as_dim_names <<- memoise::memoise(as_dim_names)
   union_dim_names <<- memoise::memoise(union_dim_names)
-  broadcast_dim_names <<- memoise::memoise(broadcast_dim_names)
+  # broadcast_dim_names <<- memoise::memoise(broadcast_dim_names)
 
   registerS3method("Ops", "ddf_col", Ops_dibble)
   registerS3method("Ops", "tbl_ddf", Ops_dibble)
